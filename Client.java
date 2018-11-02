721b5e3b14cfc0a449ab6f8a7ca001f7322f7814
venecia2 <2022>
2018-11-03 00:00:57 +0200

    modified 2

--- Client.java
+++ Client.java
@@ -1,38 +1,36 @@
 package com.company;
+
 import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
+
 
 public class Client {
 
-    public static void main(String[] args) throws InterruptedException {
-        try(Socket socket = new Socket("localhost", 10252);
-            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); )
-        {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 10252);
+             BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
             System.out.println("Client connected to socket.");
-            while(!socket.isOutputShutdown()){
-                    System.out.println("Client start writing in channel...");
-                    String clientCommand = keyboard.readLine();
-                    out.println(clientCommand);
-                    System.out.println("Client sent message \"" + clientCommand + "\" to server.");
+            while (!socket.isOutputShutdown()) {
+                System.out.println("Client start writing in channel...");
+                String clientCommand = keyboard.readLine();
+                out.println(clientCommand);
+                System.out.println("Client sent message \"" + clientCommand + "\" to server.");
 
-                    if(clientCommand.equalsIgnoreCase("quit")){
-                        System.out.println("Client kill connections");
-                        String sin = in.readLine();
-                        System.out.println(sin);
-                        break;
-                    }
-                    String sin;
-                    while(!(sin = in.readLine()).equals("End.")){
-                        System.out.println(sin);
-                    }
+                if (clientCommand.equalsIgnoreCase("quit")) {
+                    System.out.println("Client kill connections");
+                    String sin = in.readLine();
+                    System.out.println(sin);
+                    break;
+                }
+                String sin;
+                while (!(sin = in.readLine()).equals("End.")) {
+                    System.out.println(sin);
+                }
             }
             System.out.println("Closing connections & channels on clientSide - DONE.");
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Server.java
+++ Server.java
@@ -1,14 +1,15 @@
 package com.company;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class Server {
-    public static final int port = 10252;
-    public static String filepath = "text.txt";
+
+    private static final int port = 10252;
+    private static String filepath = "text.txt";
 
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(port)) {
@@ -18,43 +19,47 @@ public class Server {
             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-            while(!client.isClosed()){
+            while (!client.isClosed()) {
                 System.out.println("Server reading from channel");
                 String entry = in.readLine();
-                if(entry.equalsIgnoreCase("quit")){
-                    System.out.println("Client initialize connections suicide ...");
+                if (entry.equalsIgnoreCase("quit")) {
                     out.println("Server reply - " + entry + " - OK");
                     out.println("End.");
+                    System.out.println("Client initialize connections suicide ...");
                     break;
-                }
-                else if(entry.equalsIgnoreCase("who")){
+                } else if (entry.equalsIgnoreCase("who")) {
                     out.println("Server reply - Tanya Zvereva, Variant_10 - Context search");
                     out.println("End.");
-                    System.out.println("Server reply - " + "Tanya Zvereva, Variant_10 - Context search" + " - OK");
-                }
-                else if(entry.toLowerCase().contains("find:")) {
-
+                    System.out.println("Server reply - " + "Tanya Zvereva, Variant_10 - Context search");
+                } else if (entry.toLowerCase().contains("file to read:")) {
+                    filepath = entry.split(":")[1].trim();
+                    out.println("Server reply " + entry + " - OK");
+                    out.println("End.");
+                } else if (entry.toLowerCase().contains("find:")) {
                     String text = entry.split(":")[1].trim();
-                    HashMap<Integer, Integer> map = fileSearch(filepath, text);
+                    try {
+                        HashMap<Integer, Integer> map = fileSearch(filepath, text);
 
-                    if(map.isEmpty()){
-                        out.println("Server reply " + entry + " - OK - no matches");
-                        out.println("Server reply " + entry + " - OK - no matches");
-
-                    }
-                    else {
-                        Iterator it = map.entrySet().iterator();
-                        while (it.hasNext()){
-                            Map.Entry pair = (Map.Entry) it.next();
-                            System.out.println(String.format("Server reply - '" + text + "' was found at position %d on line %d", pair.getValue(), pair.getKey()));
-                            out.println(String.format("Server reply - \"" + text + "\" was found at position %d on line %d", pair.getValue(), pair.getKey()));
+                        if (map.isEmpty()) {
+                            out.println("Server reply " + entry + " - no matches");
+                            out.println("End.");
+                            System.out.println("Server reply " + entry + " - no matches");
+                        } else {
+                            for (Object o : map.entrySet()) {
+                                Map.Entry pair = (Map.Entry) o;
+                                System.out.println(String.format("Server reply - '" + text + "' was found at position %d on line %d", pair.getValue(), pair.getKey()));
+                                out.println(String.format("Server reply - \"" + text + "\" was found at position %d on line %d", pair.getValue(), pair.getKey()));
+                            }
+                            out.println("End.");
                         }
+                    } catch (IOException e) {
+                        out.println("Server reply - file" + filepath + " - isn't exist");
                         out.println("End.");
+                        System.out.println("Server reply - file" + filepath + " - isn't exist");
                     }
-                }
-                else{
+                } else {
                     out.println("Server reply " + entry + " - False - command doesn't exist");
-                    System.out.println("Command doesn't exist");
+                    System.out.println("Command " + entry + " doesn't exist");
                 }
 
             }
@@ -71,27 +76,22 @@ public class Server {
             e.printStackTrace();
         }
     }
-    public static HashMap <Integer,Integer> fileSearch(String filename, String textToMatch)throws FileNotFoundException{
+
+    private static HashMap<Integer, Integer> fileSearch(String filename, String textToMatch) throws IOException {
         int lineID = 0;
         String line;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "Windows-1251"));
-            while((line = bufferedReader.readLine()) != null) {
-                lineID++;
-                if (line.toLowerCase().contains(textToMatch.toLowerCase())) {
-                    int posFound = line.indexOf(textToMatch);
-                    map.put(lineID,posFound);
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "Windows-1251"));
+        while ((line = bufferedReader.readLine()) != null) {
+            lineID++;
+            if (line.toLowerCase().contains(textToMatch.toLowerCase())) {
+                int posFound = line.indexOf(textToMatch);
+                map.put(lineID, posFound);
             }
-            bufferedReader.close();
         }
-        catch(FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + filename + "'");
-        }
-        catch(IOException ex) {
-            System.out.println("Error reading file '" + filename + "'");
-        }
+        bufferedReader.close();
+
         return map;
     }
 }
